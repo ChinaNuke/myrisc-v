483172382d0580e3a0feb495fc80e752ad0c21f0
ChinaNuke <chinanuke@nuke666.cn>
2020-08-20 20:12:11 +0800

    算术运算（写了一半）

--- ex.v
+++ ex.v
@@ -32,11 +32,23 @@ module ex (
     output reg                  whilo_o
 );
 
-    reg[`RegBus] logicout;  // 逻辑运算结果
-    reg[`RegBus] shiftres;  // 移位运算结果
-    reg[`RegBus] moveres;   // 移动操作结果
-    reg[`RegBus] HI;        // 保存HI寄存器最新值
-    reg[`RegBus] LO;        // 保存LO寄存器最新值
+    reg[`RegBus] logicout;      // 逻辑运算结果
+    reg[`RegBus] shiftres;      // 移位运算结果
+    reg[`RegBus] moveres;       // 移动操作结果
+    reg[`RegBus] arithmeticres; // 算术运算结果
+    reg[`RegBus] HI;            // 保存HI寄存器最新值
+    reg[`RegBus] LO;            // 保存LO寄存器最新值
+
+    wire                ov_sum;         // 溢出标志
+    wire                reg1_eq_reg2;
+    wire                reg1_lt_reg2;
+    wire[`RegBus]       reg2_i_mux;     // reg2 的补码
+    wire[`RegBus]       reg1_i_not;     // reg1 的反码
+    wire[`RegBus]       result_sum;     // 加法结果
+    wire[`RegBus]       opdata1_mult;   // 乘法中的被乘数
+    wire[`RegBus]       opdata2_mult;   // 乘法中的乘数
+    wire[`DoubleRegBus] hilo_temp;      // 临时保存乘法结果
+    reg[`DoubleRegBus]  mulres;         // 保存乘法结果
 
     // 进行逻辑运算
     always @(*) begin 
@@ -124,10 +136,153 @@ module ex (
         end
     end
 
+    // 加减法处理
+
+    // 注意：只有减法需要考虑补码，加法不需要考虑什么补码不补码的
+    assign reg2_i_mux = ((aluop_i == `EXE_SUB_OP) || (aluop_i == `EXE_SUBU_OP) || (aluop_i == `EXE_SLT_OP)) ? 
+                        (~reg2_i) + 1 : reg2_i;
+
+    // 一步到位，实现了加、减以及比较运算，妙啊！
+    assign result_sum = reg1_i + reg2_i_mux;
+
+    // 判断溢出
+    // 溢出情况两种：1 正数之和结果为负数；2 负数之和结果为正数
+    assign ov_sum = (reg1_i[31] && reg2_i_mux[31] && !result_sum[31]) || (!reg1_i[31] && !reg2_i_mux[31] && result_sum[31])
+
+    // little than??? 这个地方是不是不对（质疑）
+    assign reg1_lt_reg2 = (aluop_i == `EXE_SLT_OP) ? 
+                                    ((reg1_i[31] && !reg2_i[31]) || 
+                                    (!reg1_i[31] && !reg2_i[31] && result_sum[31]) || 
+                                    (reg1_i[31] && reg2_i[31] && result_sum[31])) : (reg1_i < reg2_i);
+
+    assign reg1_i_not = ~reg1_i;
+
+    always @(*) begin 
+        if (rst == `RstEnable) begin
+            arithmeticres <= `ZeroWord;
+        end else begin 
+            case (aluop_i)
+                `EXE_SLT_OP, `EXE_SLTU_OP: begin 
+                    arithmeticres <= reg1_lt_reg2;
+                end
+                `EXE_ADD_OP, `EXE_ADDU_OP, `EXE_ADDI_OP, `EXE_ADDIU_OP: begin 
+                    arithmeticres <= result_sum;
+                end
+                `EXE_SUB_OP, `EXE_SUBU_OP: begin  // 为什么没有 SUBI
+                    arithmeticres <= result_sum;
+                end
+                `EXE_CLZ_OP: begin 
+                    // 太傻了这个写法
+                    arithmeticres <= reg1_i[31] ? 0 : 
+                                     reg1_i[30] ? 1 : 
+                                     reg1_i[29] ? 2 :
+                                     reg1_i[28] ? 3 :
+                                     reg1_i[27] ? 4 : 
+                                     reg1_i[26] ? 5 :
+                                     reg1_i[25] ? 6 : 
+                                     reg1_i[24] ? 7 : 
+                                     reg1_i[23] ? 8 : 
+                                     reg1_i[22] ? 9 : 
+                                     reg1_i[21] ? 10 : 
+                                     reg1_i[20] ? 11 :
+                                     reg1_i[19] ? 12 : 
+                                     reg1_i[18] ? 13 : 
+                                     reg1_i[17] ? 14 : 
+                                     reg1_i[16] ? 15 : 
+                                     reg1_i[15] ? 16 : 
+                                     reg1_i[14] ? 17 : 
+                                     reg1_i[13] ? 18 : 
+                                     reg1_i[12] ? 19 : 
+                                     reg1_i[11] ? 20 :
+                                     reg1_i[10] ? 21 : 
+                                     reg1_i[9] ? 22 : 
+                                     reg1_i[8] ? 23 : 
+                                     reg1_i[7] ? 24 : 
+                                     reg1_i[6] ? 25 : 
+                                     reg1_i[5] ? 26 : 
+                                     reg1_i[4] ? 27 : 
+                                     reg1_i[3] ? 28 : 
+                                     reg1_i[2] ? 29 : 
+                                     reg1_i[1] ? 30 : 
+                                     reg1_i[0] ? 31 : 32;
+                end
+                `EXE_CLO_OP: begin 
+                    arithmeticres <= reg1_i_not[31] ? 0 : 
+                                     reg1_i_not[30] ? 1 : 
+                                     reg1_i_not[29] ? 2 :
+                                     reg1_i_not[28] ? 3 :
+                                     reg1_i_not[27] ? 4 : 
+                                     reg1_i_not[26] ? 5 :
+                                     reg1_i_not[25] ? 6 : 
+                                     reg1_i_not[24] ? 7 : 
+                                     reg1_i_not[23] ? 8 : 
+                                     reg1_i_not[22] ? 9 : 
+                                     reg1_i_not[21] ? 10 : 
+                                     reg1_i_not[20] ? 11 :
+                                     reg1_i_not[19] ? 12 : 
+                                     reg1_i_not[18] ? 13 : 
+                                     reg1_i_not[17] ? 14 : 
+                                     reg1_i_not[16] ? 15 : 
+                                     reg1_i_not[15] ? 16 : 
+                                     reg1_i_not[14] ? 17 : 
+                                     reg1_i_not[13] ? 18 : 
+                                     reg1_i_not[12] ? 19 : 
+                                     reg1_i_not[11] ? 20 :
+                                     reg1_i_not[10] ? 21 : 
+                                     reg1_i_not[9] ? 22 : 
+                                     reg1_i_not[8] ? 23 : 
+                                     reg1_i_not[7] ? 24 : 
+                                     reg1_i_not[6] ? 25 : 
+                                     reg1_i_not[5] ? 26 : 
+                                     reg1_i_not[4] ? 27 : 
+                                     reg1_i_not[3] ? 28 : 
+                                     reg1_i_not[2] ? 29 : 
+                                     reg1_i_not[1] ? 30 : 
+                                     reg1_i_not[0] ? 31 : 32;
+                end
+                default : begin 
+                    arithmeticres <= `ZeroWord;
+                end
+            endcase
+        end
+    end
+
+    // 乘法处理
+
+    assign opdata1_mult = ((aluop_i == `EXE_MUL_OP || aluop_i == `EXE_MULT_OP) && reg1_i[31]) ? (~reg1_i + 1) : reg1_i;
+    assign opdata2_mult = ((aluop_i == `EXE_MUL_OP || aluop_i == `EXE_MULT_OP) && reg2_i[31]) ? (~reg2_i + 1) : reg2_i;
+
+    assign hilo_temp = opdata1_mult * opdata2_mult;
+
+    // 对除法结果进行修正
+    always @(*) begin 
+        if (rst == `RstEnable) begin
+            mulres <= {`ZeroWord, `ZeroWord};
+        end else if (aluop_i == `EXE_MULT_OP || aluop_i == `EXE_MUL_OP) begin
+            // 有符号乘法，正负得负
+            if (reg1_i[31] ^ reg2_i[31] == 1'b1) begin
+                mulres <= ~hilo_temp + 1;
+            // 有符号乘法同号的情况
+            end else begin 
+                mulres <= hilo_temp;
+            end
+        end else begin 
+            // 无符号乘法
+            mulres <= hilo_temp;
+        end
+    end
+
     // 根据 alusel_i 选择最终运算结果
     always @(*) begin 
         wd_o    <= wd_i;
-        wreg_o  <= wreg_i;
+
+        // 溢出则不写入寄存器
+        if ((aluop_i == `EXE_ADD_OP || aluop_i == `EXE_ADDI_OP || aluop_i == `EXE_SUB_OP) && ov_sum == 1'b1) begin
+            wreg_o <= `WriteDisable;
+        end else begin 
+            wreg_o  <= wreg_i;
+        end
+        
         case (alusel_i)
             `EXE_RES_LOGIC: begin 
                 wdata_o <= logicout;
@@ -137,6 +292,12 @@ module ex (
             end
             `EXE_RES_MOVE: begin 
                 wdata_o <= moveres;
+            end
+            `EXE_RES_ARITHMETIC: begin 
+                wdata_o <= arithmeticres;
+            end
+            `EXE_RES_MUL: begin 
+                wdata_o <= mulres[31:0];
             end
             default : begin 
                 wdata_o <= `ZeroWord;
